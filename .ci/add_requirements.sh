cb0c9a93accf43b0afabea7c952b8b24cb8db82a
Ben <xion03@gmail.com>
2018-10-16 17:58:24 +0200

    removed coverage from pipeline

---
+++ .ci/add_requirements.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+if ! git diff --exit-code --name-only development.requirements requirements.txt > /dev/null; then
+  git config --global user.email droneci
+  git config --global user.email ci@wohlben.de
+  git add -f development.requirements requirements.txt
+  git commit -m "added requirements"
+  git push origin
+fi
