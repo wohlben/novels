82e39da248d144c4f81eaa209094af8812290f82
wohlben <bw@wohlben.de>
2018-10-31 17:02:12 +0100

    fix

--- .ci/add_requirements.sh
+++ .ci/add_requirements.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 
+git config --global user.email droneci
+git config --global user.email ci@wohlben.de
+
+if [[ "$(git remote get-url origin)" == "https://github.com/wohlben/novels.git" ]]; then
+  echo  "fixing remote"
+  git remote set-url origin git@github.com:wohlben/novels.git
+fi
+
 if ! git diff --exit-code --name-only development.requirements requirements.txt > /dev/null; then
-  git config --global user.email droneci
-  git config --global user.email ci@wohlben.de
+  echo "found new requirements, commit them"
   git add -f development.requirements requirements.txt
   git commit -m "updated requirements"
-  git push origin
+  git push origin $DRONE_COMMIT_BRANCH
 fi
