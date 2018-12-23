43607ee77283144e9712b82868a2a320e8bd7670
wohlben <bw@wohlben.de>
2018-12-23 11:28:01 +0100

    moved a the rrl classes into their own file

---
+++ scrapes/managers/RRL.py
@@ -0,0 +1,13 @@
+from . import ScrapeManager
+from .rrl_novel import RRLNovelParserMixin, RRLNovelGeneratorMixin
+from .rrl_latest import RRLLatestParserMixin, RRLLatestGeneratorMixin
+from .rrl_chapter import RRLChapterGeneratorMixin, RRLChapterParserMixin
+
+class RRLNovelScraper(ScrapeManager, RRLNovelGeneratorMixin, RRLNovelParserMixin):
+    parser_name = "rrl novel"
+
+class RRLLatestScraper(ScrapeManager, RRLLatestGeneratorMixin, RRLLatestParserMixin):
+    parser_name = "rrl latest"
+
+class RRLChapterScraper(ScrapeManager, RRLChapterGeneratorMixin, RRLChapterParserMixin):
+    parser_name = "rrl chapter"
--- scrapes/managers/__init__.py
+++ scrapes/managers/__init__.py
@@ -1,19 +1,6 @@
 from .ScrapeManager import ScrapeManager
-from .rrl_novel import RRLNovelParserMixin, RRLNovelGeneratorMixin
-from .rrl_latest import RRLLatestParserMixin, RRLLatestGeneratorMixin
-from .rrl_chapter import RRLChapterGeneratorMixin, RRLChapterParserMixin
-
+from .RRL import RRLLatestScraper, RRLNovelScraper, RRLChapterScraper
 __all__ = ["Managers", "RRLNovelScraper", "RRLLatestScraper", "RRLChapterScraper", "ScrapeManager"]
-
-
-class RRLNovelScraper(ScrapeManager, RRLNovelGeneratorMixin, RRLNovelParserMixin):
-    parser_name = "rrl novel"
-
-class RRLLatestScraper(ScrapeManager, RRLLatestGeneratorMixin, RRLLatestParserMixin):
-    parser_name = "rrl latest"
-
-class RRLChapterScraper(ScrapeManager, RRLChapterGeneratorMixin, RRLChapterParserMixin):
-    parser_name = "rrl chapter"
 
 class Managers(object):
     def __init__(self, *args, **kwargs):
