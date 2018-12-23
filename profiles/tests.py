f75be89809370b2c0de819390babf1be4d6151a9
wohlben <bw@wohlben.de>
2018-12-23 13:11:17 +0100

    added login view test

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class LoginViewTests(TestCase):
+
+    def test_unauthenticated_get(self):
+        response = self.client.get(reverse('login'))
+        self.assertContains(response, "Login with GitHub")
+        self.assertEqual(response.status_code, 200)
