15d7c4e96c4f17c7a634652431acd5a7e3499cb2
wohlben <bw@wohlben.de>
2018-12-26 18:32:33 +0100

    added token login test

--- profiles/tests.py
+++ profiles/tests.py
@@ -4,10 +4,31 @@ from .models import User
 
 
 class LoginViewTests(TestCase):
+    def setUp(self):
+        self.user = User.objects.get_or_create(
+            username="testuser", defaults={"enable_login_token": True}
+        )[0]
+
     def test_unauthenticated_get(self):
         response = self.client.get(reverse("login"))
         self.assertContains(response, "Login with GitHub")
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            response.status_code, 200, "response should be a simple http ok"
+        )
+
+    def test_login_token_login(self):
+        response = self.client.get(
+            f"{reverse('login')}?login_token={self.user.login_token}"
+        )
+        self.assertEqual(
+            response.status_code, 302, "response should be a redirect to home"
+        )
+        self.assertRedirects(
+            response,
+            reverse("home"),
+            target_status_code=302,
+            msg_prefix="sucessfull login should redirect to home",
+        )
 
 
 class ProfileViewTests(TestCase):
