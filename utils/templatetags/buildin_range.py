4104c96920a9bd1df8f87b911d761857e48e603e
wohlben <bw@wohlben.de>
2018-12-27 19:32:05 +0100

    removed unnecessary templatetag, replaced with js method

--- utils/templatetags/buildin_range.py
+++ utils/templatetags/buildin_range.py
@@ -4,36 +4,5 @@ register = template.Library()  # pragma: no cover
 
 
 @register.filter(name="range")
-def buildin_range(stop, start):
-    return range(start, stop)
-
-
-@register.simple_tag(takes_context=True)
-def param_replace(context, **kwargs):
-    """
-    Return encoded URL parameters that are the same as the current
-    request's parameters, only with the specified GET parameters added or changed.
-
-    It also removes any empty parameters to keep things neat,
-    so you can remove a parm by setting it to ``""``.
-
-    For example, if you're on the page ``/things/?with_frosting=true&page=5``,
-    then
-
-    <a href="/things/?{% param_replace page=3 %}">Page 3</a>
-
-    would expand to
-
-    <a href="/things/?with_frosting=true&page=3">Page 3</a>
-
-    Based on
-    https://stackoverflow.com/questions/22734695/next-and-before-links-for-a-django-paginated-query/22735278#22735278
-    """
-    d = context["request"].GET.copy()
-    print(d)
-    for k, v in kwargs.items():
-        d[k] = v
-    for k in [k for k, v in d.items() if not v]:
-        del d[k]
-    print(d)
-    return d.urlencode()
+def buildin_range(stop):
+    return range(stop)
