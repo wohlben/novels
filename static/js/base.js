dfbb0bffeee133dc0cbfa291c44a333baef56380
wohlben <bw@wohlben.de>
2018-12-27 19:37:16 +0100

    removed delete js method and replaced it with optional set value

--- static/js/base.js
+++ static/js/base.js
@@ -5,21 +5,14 @@
 //   });
 // });
 
-function setGetParam(key, value){
+function setGetParam(key, value = undefined){
     var url = new URL(window.location);
     var query_string = url.search;
-    var search_params = new URLSearchParams(query_string); 
+    var search_params = new URLSearchParams(query_string);
     search_params.delete(key);
-    search_params.append(key, value);
-    url.search = search_params.toString();
-    var new_url = url.toString();
-    window.location.href = new_url;
-};
-function deleteGetParam(key){
-    var url = new URL(window.location);
-    var query_string = url.search;
-    var search_params = new URLSearchParams(query_string); 
-    search_params.delete(key);
+    if(value !== undefined){
+      search_params.append(key, value);
+    };
     url.search = search_params.toString();
     var new_url = url.toString();
     window.location.href = new_url;
