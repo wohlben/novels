04c2a7112077d81e6a1af66a1651a0d8f0e03f65
wohlben <bw@wohlben.de>
2018-12-28 12:32:57 +0100

    refactored the rrl_chapter parser to directly access db for easier testing

--- scrapes/managers/rrl_chapter/parser.py
+++ scrapes/managers/rrl_chapter/parser.py
@@ -1,7 +1,7 @@
 """discovery for pending rrl latest parses and parsing them into fictions and novels."""
 from lxml import html
-from scrapes.models import ParseLog
-from novels.models import Chapter
+from scrapes.models import ParseLog, Scrapes
+from novels.models import Chapter, Fiction
 from django.utils import timezone
 from datetime import datetime
 import re
@@ -14,7 +14,7 @@ class RRLChapterParserMixin(object):
 
     def chapter_extractor(self):
         """Return False if no Parses were necessary. True if the parsind was successful."""
-        pending_parses = self.all_pending_parses()
+        pending_parses = self.all_pending_parses().values("id")
 
         if pending_parses.count() == 0:
             self.logger.info("no rrl chapter page to parse")
@@ -22,24 +22,9 @@ class RRLChapterParserMixin(object):
 
         self.logger.info(f"found {pending_parses.count()} chapter scrapes to parse!")
 
-        success_monitor = True
-
         for scrape in pending_parses:
-            self.logger.info(f"parsing {scrape.id}")
-
-            tree = html.fromstring(scrape.content)
-
-            parse_log = ParseLog.objects.create(
-                scrape=scrape, parser_id=self.get_parser_id(), started=timezone.now()
-            )
-
-            data_extracted = self._parse_chapter_page(tree, scrape.url)
-            parse_log.finished = timezone.now()
-            if data_extracted:
-                parse_log.success = success_monitor
-            else:
-                success_monitor = False  # pragma: no cover
-            parse_log.save()
+            self.logger.info(f"parsing {scrape['id']}")
+            self.parse_chapter_scrape(scrape["id"])
 
         return True
 
@@ -50,31 +35,59 @@ class RRLChapterParserMixin(object):
             .encode("raw_unicode_escape")
             .decode("utf-8")
         )
-        removedScripts = re.sub(r"<script.*?</script>", "", str(content))
-        return removedScripts
+        removed_scripts = re.sub(r"<script.*?</script>", "", str(content))
+        return removed_scripts
 
-    def _parse_chapter_page(self, element, url):
+    def parse_chapter_scrape(self, scrape_id: int) -> bool:
+        scrape = Scrapes.objects.get(id=scrape_id)
+        chapter = Chapter.objects.get(url=scrape.url)  # TODO: get or create
+        parse_log = ParseLog.objects.create(
+            scrape=scrape,
+            parser_id=self.get_parser_id(),
+            started=timezone.now(),
+            modified_object={
+                "count": 1,
+                "chapters": [chapter.id],
+                "types": ["chapters"],
+            },
+        )
         try:
-            remote_id = url.split("/")[-2]
-            chap = Chapter.objects.get(url=url)
-            chap.content = self._clean_chapter_content(
-                tostring(element.cssselect(".chapter-content")[0])
-            )
-            if chap.remote_id is None:
-                chap.remote_id = remote_id  # pragma: no cover
-            if chap.remote_id != remote_id:  # pragma: no cover
+            tree = html.fromstring(scrape.content)
+
+            chapter_content = ""
+            for i in tree.cssselect(".chapter-content > *"):
+                chapter_content += self._clean_chapter_content(tostring(i))
+            chapter.content = chapter_content
+
+            remote_id = scrape.url.split("/")[-2]
+            if chapter.remote_id is None:
+                chapter.remote_id = remote_id
+            if chapter.remote_id != remote_id:
                 self.logger.error(
                     "unexpected remote_id. not updating content on possible parsing error!"
                 )
-                return False
-            timestamp = int(
-                element.xpath('//i[@title="Published"]/../time/@unixtime')[0]
-            )
-            chap.published = timezone.make_aware(
+                raise Exception("unexpected Data")
+
+            timestamp = int(tree.xpath('//i[@title="Published"]/../time/@unixtime')[0])
+            chapter.published = timezone.make_aware(
                 datetime.utcfromtimestamp(timestamp), timezone.utc
             )
-            chap.save()
-            self.logger.info(f'updated content of "{chap.title}"')
-            return True
+
+            fiction_remote_id = int(scrape.url.split("/")[4])
+            fiction = Fiction.objects.get(remote_id=fiction_remote_id)
+
+            chapter.fiction = fiction
+            chapter.save()
+
+            self.logger.info(
+                f'updated content of "{chapter.fiction.title}: {chapter.title}"'
+            )
+
+            parse_log.finished = timezone.now()
+            parse_log.success = True
+            parse_log.save()
         except Exception:  # pragma: no cover
-            self.logger.exception(f"failed to parse chapter from {url}")
+            self.logger.exception(f"failed to parse chapter from {scrape.url}")
+            parse_log.success = False
+            parse_log.save()
+            return False
