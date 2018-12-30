89765e4a8909fb060c22da249dd63342512c974c
wohlben <bw@wohlben.de>
2018-12-30 14:54:25 +0100

    temporarely added a few more cleaning steps to chapter detail.

--- utils/templatetags/read_inserter.py
+++ utils/templatetags/read_inserter.py
@@ -6,19 +6,31 @@ from django.urls import reverse
 register = template.Library()  # pragma: no cover
 
 
+def div_builder(progress, chapter_id):
+    url = reverse(
+        "profiles:reading-progress",
+        kwargs={"chapter_id": chapter_id, "progress": progress},
+    )
+    div = f'<div id="progress-{progress}" ic-get-from="{url}" ic-trigger-on="scrolled-into-view" ic-replace-target="true"></div>'
+    return div
+
+
 @register.filter(name="read_inserter")
 def read_inserter(html_string, chapter_id):
 
     content = ""
     tree = html.fromstring(html_string)
+    for anchor in tree.xpath("//a"):
+        anchor.drop_tag()
+    for script in tree.xpath("//script"):
+        script.drop_tag()
+    for span in tree.xpath("//*[@lang]"):
+        span.drop_tag()
     if tree.text is not None and tree.text.strip() != "":
         content += tree.text
-    for index, element in enumerate(tree.getchildren()):
+    children = tree.getchildren()
+    for index, element in enumerate(children, 1):
         content += tostring(element).decode("unicode-escape")
-        url = reverse(
-            "profiles:reading-progress",
-            kwargs={"chapter_id": chapter_id, "progress": index},
-        )
         if index % 10 == 0:
-            content += f'<div id="progress-{index}" ic-get-from="{url}" ic-trigger-on="scrolled-into-view" ic-replace-target="true"></div>'
+            content += div_builder(index, chapter_id)
     return content
