78d2be84f785cd4c8bee82891f2243f3f1ad3abc
wohlben <bw@wohlben.de>
2018-12-30 11:11:28 +0100

    switched even more imports to private interfaces according to pep8

--- scrapes/models.py
+++ scrapes/models.py
@@ -1,9 +1,9 @@
 """Database definitions for the Scrapes app."""
-from django.db import models
-from django.contrib.postgres.fields import JSONField
+from django.db import models as _models
+from django.contrib.postgres.fields import JSONField as _JSONField
 
 
-class Scrapes(models.Model):
+class Scrapes(_models.Model):
     """Urls that should be Scraped."""
 
     class Meta:
@@ -12,41 +12,41 @@ class Scrapes(models.Model):
             ("view_system", "Allows the User to see the System views"),
         )
 
-    url = models.TextField(blank=False)
-    timestamp = models.DateTimeField(auto_now_add=True)
-    last_change = models.DateTimeField(auto_now=True)
-    content = models.TextField(blank=True, null=True)
-    http_code = models.IntegerField(blank=True, null=True)
-    parser_type = models.ForeignKey("Parser", on_delete=models.CASCADE)
-    added_by = models.ForeignKey(
+    url = _models.TextField(blank=False)
+    timestamp = _models.DateTimeField(auto_now_add=True)
+    last_change = _models.DateTimeField(auto_now=True)
+    content = _models.TextField(blank=True, null=True)
+    http_code = _models.IntegerField(blank=True, null=True)
+    parser_type = _models.ForeignKey("Parser", on_delete=_models.CASCADE)
+    added_by = _models.ForeignKey(
         "ParseLog",
         null=True,
         blank=True,
         related_name="added_by",
-        on_delete=models.SET_NULL,
+        on_delete=_models.SET_NULL,
     )
-    added_reason = models.TextField(blank=True, null=True)
+    added_reason = _models.TextField(blank=True, null=True)
 
 
-class Parser(models.Model):
+class Parser(_models.Model):
     """Category of Scrapes and which parser should be applied."""
 
-    name = models.TextField()
-    added = models.DateTimeField(auto_now_add=True)
-    url_scheme = models.TextField(blank=True)
-    weight = models.IntegerField(default=50)
+    name = _models.TextField()
+    added = _models.DateTimeField(auto_now_add=True)
+    url_scheme = _models.TextField(blank=True)
+    weight = _models.IntegerField(default=50)
 
     @property
     def pending(self):
         return Scrapes.objects.filter(parser_type=self, http_code=None)
 
 
-class ParseLog(models.Model):
+class ParseLog(_models.Model):
     """Log of all Parses."""
 
-    scrape = models.ForeignKey("Scrapes", on_delete=models.CASCADE)
-    parser = models.ForeignKey("Parser", on_delete=models.CASCADE)
-    started = models.DateTimeField(blank=True, null=True)
-    finished = models.DateTimeField(blank=True, null=True)
-    success = models.BooleanField(default=False)
-    modified_object = JSONField(blank=True, null=True)
+    scrape = _models.ForeignKey("Scrapes", on_delete=_models.CASCADE)
+    parser = _models.ForeignKey("Parser", on_delete=_models.CASCADE)
+    started = _models.DateTimeField(blank=True, null=True)
+    finished = _models.DateTimeField(blank=True, null=True)
+    success = _models.BooleanField(default=False)
+    modified_object = _JSONField(blank=True, null=True)
