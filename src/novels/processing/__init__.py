3ea883d518990194057e1478e600bac0f11f411c
wohlben <bw@wohlben.de>
2019-01-19 10:32:27 +0100

    fix ci without spacy models

--- src/novels/processing/__init__.py
+++ src/novels/processing/__init__.py
@@ -1,4 +1,9 @@
-from .TextRanker import TextRanker
+from os import environ as _environ
 
+if _environ.get("CI") == "true":
+    print("processins is unavailable in CI")
+    __all__ = []
+else:
+    from .TextRanker import TextRanker
 
-__all__ = ["TextRanker"]
+    __all__ = ["TextRanker"]
