84177b886f7167aabebf1c47d37b56b7fc117e5c
wohlben <bw@wohlben.de>
2019-06-01 17:41:22 +0200

    fixed content fetch etc

--- src/scrapes/tasks.py
+++ src/scrapes/tasks.py
@@ -1,6 +1,7 @@
 from celery import shared_task
 from requests import get
 from scrapes.managers import Managers
+from scrapes.models import Scrapes
 import logging
 
 logger = logging.getLogger("scrapes.tasks")
@@ -9,10 +10,15 @@ managers = Managers()
 
 
 @shared_task
-def fetch_content():  # TODO: mock response..... # TODO: dont fetch if another Scrape < 15 min was done to the same url
+def fetch_content(
+    scrape_id=None
+):  # TODO: mock response..... # TODO: dont fetch if another Scrape < 15 min was done to the same url
     """Fetch an URL from a remote server."""
     try:
-        instance = managers.manager.scrape_queue().first()
+        if scrape_id is None:
+            instance = managers.manager.scrape_queue().first()
+        else:
+            instance = Scrapes.objects.get(id=scrape_id)
         if not instance:
             logger.info("no pending scrapes")
             return True
@@ -20,13 +26,16 @@ def fetch_content():  # TODO: mock response..... # TODO: dont fetch if another S
         logger.info(f"fetching {instance.url} for {instance.id}")
 
         with get(instance.url) as page:
-            instance.content = page.content
+            instance.content = page.text
             instance.http_code = page.status_code
             instance.save()
 
             logger.info(f"finished {instance.id} with https_code {instance.http_code}")
 
-        getattr(managers, instance.parser_type.name.replace(" ", "_")).parse()
+        try:
+            getattr(managers, instance.parser_type.name.replace(" ", "_")).parse()
+        except Exception:
+            logger.error(f"couldn't trigger the parse for {instance.parser_type.name}")
         return True
     except Exception:
         logger.exception(f"failed to fetch content for {instance.url}")
